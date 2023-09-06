98c3ed359e6eb32a2f1eab247a9832ee97d0909d
Swapnil Bhakare <2191560@tcs.com>
2023-09-06 07:14:09 +0530

    Signup Screen done

---
+++ src/Components/Auth/Authentication.js
@@ -0,0 +1,150 @@
+import React, { useRef, useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Col, Form, Row, Button, FloatingLabel, Container } from "react-bootstrap";
+import { BiSolidLockAlt } from "react-icons/bi";
+import styleshhet from './Authentication.module.css'
+
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+// AIzaSyBSRUpGLF7ibaidLOQigcjjDfdz-vXDTsU
+
+ 
+const Authentication = () => {
+  const navigate = useNavigate();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const confirmPasswordInputRef = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+
+ const submitHandler =(event)=>{
+  event.preventDefault()
+  const enterdEmail = emailInputRef.current.value
+  const enterdPassword = passwordInputRef.current.value
+  const enterdConfirmPassword = confirmPasswordInputRef.current.value
+
+  let errorMessage
+    let url= "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBSRUpGLF7ibaidLOQigcjjDfdz-vXDTsU"
+
+  if(enterdEmail && enterdPassword && enterdConfirmPassword){
+    if(enterdPassword !== enterdConfirmPassword){
+      alert("Please check Password")
+
+    }else{
+        fetch(url,{
+          method:"POST",
+          body:JSON.stringify({
+            email:enterdEmail,
+            password:enterdPassword,
+            confirmPassword:enterdConfirmPassword,
+            returnSecureToken:true
+          }),
+          headers:{
+            "Content-Type": "application/json",
+          }
+        }).then((res)=>{
+          if(res.ok){
+            return res.json()
+          }else{
+            return res.json().then((data)=>{
+              errorMessage = "Authentication Failed !"
+              throw new Error(errorMessage)
+            })
+          }
+        }).then((data)=>{
+          console.log(data)
+        }).catch((err)=>{
+          alert(err.message)
+        })
+      }
+
+
+  }else{
+    alert("Please enter valid details")
+  }
+ }
+    
+
+  
+  
+  return (
+    <Container>
+    <Form onSubmit={submitHandler}>
+      
+
+      
+      <Row  className="d-flex p-3  flex-column shadow align-items-center mx-auto mt-5" style={{width:"400px"}}>
+        <Col>
+          <h2 className="p-1 mb-3 center">
+            <BiSolidLockAlt   className="rounded-circle p-1 text-white bg-info" /> Sign Up
+          </h2>
+        </Col>
+
+        <Col >
+          <Form.Group>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Email address"
+              className="mb-3"
+            >
+              <Form.Control
+                className={`${styleshhet['form-control']} border-0 border-bottom rounded-0 `}
+                type="email"
+                placeholder="Enter Email"
+                ref={emailInputRef}
+                required
+              />
+            </FloatingLabel>
+          </Form.Group>
+
+          <Form.Group>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Enter password"
+              className="mb-3"
+            >
+              <Form.Control
+                className={` ${styleshhet['form-control']} border-0 border-bottom rounded-0 `}
+                type="password"
+                placeholder="Enter password"
+                ref={passwordInputRef}
+                required
+              />
+            </FloatingLabel>
+          </Form.Group>
+
+          <Form.Group>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Confirm password"
+              className="mb-3"
+            >
+              <Form.Control
+                className={` ${styleshhet['form-control']} border-0 border-bottom rounded-0 `}
+                type="password"
+                placeholder="Confirm password"
+                ref={confirmPasswordInputRef}
+                required
+              />
+            </FloatingLabel>
+          </Form.Group>
+
+          <Form.Group>
+            <Button variant="outline-info" size="lg" className={styleshhet['submit-btn']} type="submit">Sign Up</Button>
+          </Form.Group>
+
+          {/* <Form.Group as={Col}>
+            <Button> Forget Password ?</Button>
+          </Form.Group> */}
+
+        </Col>
+      </Row>
+
+    </Form>
+    <Button type="button" variant="outline-info"  className={`${styleshhet['button']} mt-5`}>Have an Account? Login</Button>
+
+    </Container>
+  );
+};
+
+export default Authentication;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// react Boostrap config
+import { ToastContainer } from 'react-toastify';
+import '../node_modules/react-bootstrap/dist/react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+ <BrowserRouter>
+ <ToastContainer />
+ <App />
+ </BrowserRouter>
   </React.StrictMode>
 );
 
